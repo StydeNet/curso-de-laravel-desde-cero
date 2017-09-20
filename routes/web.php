c6467bccd7af23e0f4f64da196e353ccc575ba54
Duilio Palacios <duilio@styde.net>
2017-09-20 11:10:48 +0100

    Route examples

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+   return 'Home';
+});
+
+Route::get('/usuarios', function () {
+    return 'Usuarios';
+});
+
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo usuario';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "Mostrando detalle del usuario: {$id}";
+});
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    if ($nickname) {
+        return "Bienvenido {$name}, tu apodo es {$nickname}";
+    } else {
+        return "Bienvenido {$name}, no tienes apodo";
+    }
 });
