c94c24a323410cf8ab8f0f957037b19aefae212a
Duilio Palacios <duilio@styde.net>
2017-11-16 12:12:51 +0000

    Change profession to profession_id in the users table, add professions table

--- database/migrations/2017_11_15_173441_create_professions_table.php
+++ database/migrations/2017_11_15_173441_create_professions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProfessionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('professions', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->string('title', 100);
+
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('professions');
     }
 }
--- database/migrations/2017_11_15_181414_add_profession_id_to_users.php
+++ database/migrations/2017_11_15_181414_add_profession_id_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddProfessionToUsers extends Migration
+class AddProfessionIdToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddProfessionToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profession', 50)->nullable()->after('password');
+            $table->unsignedInteger('profession_id');
+            $table->foreign('profession_id')->references('id')->on('professions');
         });
     }
 
@@ -25,8 +26,9 @@ class AddProfessionToUsers extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('profession');
-        });
+//        Schema::table('users', function (Blueprint $table) {
+//            $table->dropForeign(['profession_id']);
+//            $table->dropColumn('profession_id');
+//        });
     }
 }
