93f851476e54426957c3b0b77d33ede46c2d76ba
Duilio Palacios <duilio@styde.net>
2017-11-20 18:26:25 +0000

    Link seeded user to back-end profession

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -12,6 +12,10 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
+//        DB::insert('INSERT INTO professions (title) VALUES (:title)', [
+//            'title' => 'Desarrollador back-end',
+//        ]);
+
         DB::table('professions')->insert([
             'title' => 'Desarrollador back-end',
         ]);
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,10 +12,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        //$professions = DB::select('SELECT id FROM professions WHERE title = ?', ['Desarrollador back-end']);
+
+        $professionId = DB::table('professions')
+            ->where('title', 'Desarrollador back-end')
+            ->value('id');
+
         DB::table('users')->insert([
             'name' => 'Duilio Palacios',
             'email' => 'duilio@styde.net',
             'password' => bcrypt('laravel'),
+            'profession_id' => $professionId
         ]);
     }
 }
