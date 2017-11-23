6c6960e322c6b7bb9615bcca7a9ef68f0b87043b
Duilio Palacios <duilio@styde.net>
2017-11-23 11:48:47 +0000

    Add methods to the user model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static function findByEmail($email)
+    {
+        return static::where(compact('email'))->first();
+    }
+
+    public function isAdmin()
+    {
+        return $this->email === 'duilio@styde.net';
+    }
 }
