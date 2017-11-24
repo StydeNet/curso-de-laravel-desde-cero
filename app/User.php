26a42c1913b8e95fa6a5175f8b53babee82a1e18
Duilio Palacios <duilio@styde.net>
2017-11-24 11:32:46 +0000

    Add is_admin attribute to users

--- app/User.php
+++ app/User.php
@@ -27,6 +27,10 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    protected $casts = [
+        'is_admin' => 'boolean'
+    ];
+
     public static function findByEmail($email)
     {
         return static::where(compact('email'))->first();
--- database/migrations/2014_10_12_100000_create_users_table.php
+++ database/migrations/2014_10_12_100000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
