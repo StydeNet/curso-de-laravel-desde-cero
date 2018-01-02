250480268ef0f39716cc96e87ae826ec5db226f8
Duilio Palacios <duilio@styde.net>
2018-01-02 16:53:24 +0000

    Add Route Model Binding

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,10 +22,8 @@ class UserController extends Controller
         return view('users.index', compact('title', 'users'));
     }
 
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
-
         return view('users.show', compact('user'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::get('/', function () {
 Route::get('/usuarios', 'UserController@index')
     ->name('users.index');
 
-Route::get('/usuarios/{id}', 'UserController@show')
-    ->where('id', '[0-9]+')
+Route::get('/usuarios/{user}', 'UserController@show')
+    ->where('user', '[0-9]+')
     ->name('users.show');
 
 Route::get('/usuarios/nuevo', 'UserController@create')->name('users.create');
