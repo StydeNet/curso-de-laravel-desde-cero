cb4a7dfe32bab48fb89a757c7f351d321c37b471
Duilio Palacios <duilio@styde.net>
2018-01-09 13:21:05 +0000

    Show validation errors and preserve form data

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,14 +5,28 @@
 @section('content')
     <h1>Crear usuario</h1>
 
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <h6>Por favor corrige los errores debajo:</h6>
+        {{--<ul>--}}
+            {{--@foreach ($errors->all() as $error)--}}
+                {{--<li>{{ $error }}</li>--}}
+            {{--@endforeach--}}
+        {{--</ul>--}}
+    </div>
+    @endif
+
     <form method="POST" action="{{ url('usuarios') }}">
         {{ csrf_field() }}
 
         <label for="name">Nombre:</label>
-        <input type="text" name="name" id="name" placeholder="Pedro Perez">
+        <input type="text" name="name" id="name" placeholder="Pedro Perez" value="{{ old('name') }}">
+        @if ($errors->has('name'))
+            <p>{{ $errors->first('name') }}</p>
+        @endif
         <br>
         <label for="email">Correo electrónico:</label>
-        <input type="email" name="email" id="email" placeholder="pedro@example.com">
+        <input type="email" name="email" id="email" placeholder="pedro@example.com" value="{{ old('email') }}">
         <br>
         <label for="password">Contraseña:</label>
         <input type="password" name="password" id="password" placeholder="Mayor a 6 caracteres">
