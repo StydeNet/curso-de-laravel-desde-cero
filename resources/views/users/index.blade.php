e5edf4aad40c63f616fcbe2c079d585f12f0afa0
Duilio Palacios <duilio@styde.net>
2018-01-24 16:46:20 +0000

    Improve styles in the users list

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,28 +3,45 @@
 @section('title', 'Usuarios')
 
 @section('content')
-    <h1>{{ $title }}</h1>
+    <div class="d-flex justify-content-between align-items-end mb-3">
+        <h1 class="pb-1">{{ $title }}</h1>
+        <p>
+            <a href="{{ route('users.create') }}" class="btn btn-primary">Nuevo usuario</a>
+        </p>
+    </div>
 
-    <p>
-        <a href="{{ route('users.create') }}">Nuevo usuario</a>
-    </p>
-
-    <ul>
-        @forelse ($users as $user)
-            <li>
-                {{ $user->name }}, ({{ $user->email }})
-                <a href="{{ route('users.show', $user) }}">Ver detalles</a> |
-                <a href="{{ route('users.edit', $user) }}">Editar</a> |
+    @if ($users->isNotEmpty())
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Acciones</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+        <tr>
+            <th scope="row">{{ $user->id }}</th>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>
                 <form action="{{ route('users.destroy', $user) }}" method="POST">
                     {{ csrf_field() }}
                     {{ method_field('DELETE') }}
-                    <button type="submit">Eliminar</button>
+                    <a href="{{ route('users.show', $user) }}" class="btn btn-link"><span class="oi oi-eye"></span></a>
+                    <a href="{{ route('users.edit', $user) }}" class="btn btn-link"><span class="oi oi-pencil"></span></a>
+                    <button type="submit" class="btn btn-link"><span class="oi oi-trash"></span></button>
                 </form>
-            </li>
-        @empty
-            <li>No hay usuarios registrados.</li>
-        @endforelse
-    </ul>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+    @else
+        <p>No hay usuarios registrados.</p>
+    @endif
 @endsection
 
 @section('sidebar')
