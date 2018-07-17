1267f95f37250d8d36551e9d43f76e5b01f658ef
Duilio Palacios <duilio@styde.net>
2018-07-17 17:03:44 +0100

    include card component

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,20 +3,18 @@
 @section('title', "Crear usuario")
 
 @section('content')
-    <div class="card">
-        <h4 class="card-header">Crear usuario</h4>
-        <div class="card-body">
+    @card
+        @slot('header', 'Nuevo usuario')
 
-            @include('shared._errors')
+        @include('shared._errors')
 
-            <form method="POST" action="{{ url('usuarios') }}">
-                @include('users._fields')
+        <form method="POST" action="{{ url('usuarios') }}">
+            @include('users._fields')
 
-                <div class="form-group mt-4">
-                    <button type="submit" class="btn btn-primary">Crear usuario</button>
-                    <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
-                </div>
-            </form>
-        </div>
-    </div>
+            <div class="form-group mt-4">
+                <button type="submit" class="btn btn-primary">Crear usuario</button>
+                <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
+            </div>
+        </form>
+    @endcard
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,18 +3,20 @@
 @section('title', "Crear usuario")
 
 @section('content')
-    <h1>Editar usuario</h1>
+    @card
+        @slot('header', 'Editar usuario')
 
-    @include('shared._errors')
+        @include('shared._errors')
 
-    <form method="POST" action="{{ url("usuarios/{$user->id}") }}">
-        {{ method_field('PUT') }}
+        <form method="POST" action="{{ url("usuarios/{$user->id}") }}">
+            {{ method_field('PUT') }}
 
-        @include('users._fields')
+            @include('users._fields')
 
-        <div class="form-group mt-4">
-            <button type="submit" class="btn btn-primary">Actualizar usuario</button>
-            <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
-        </div>
-    </form>
+            <div class="form-group mt-4">
+                <button type="submit" class="btn btn-primary">Actualizar usuario</button>
+                <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
+            </div>
+        </form>
+    @endcard
 @endsection
