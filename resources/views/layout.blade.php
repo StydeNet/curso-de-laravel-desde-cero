9dd66c9a94a5d11d84465deb81fe1e5d9972dd5f
Ramon Lapenta <ramono@gmail.com>
2018-08-04 12:59:52 -0600

    Agregando front end para filtros"

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -44,11 +44,11 @@
 <!-- Begin page content -->
 <main role="main" class="container">
     <div class="row mt-3">
-        <div class="col-8">
+        <div class="col-9">
             @yield('content')
         </div>
-        <div class="col-4">
-            <p>&nbsp;</p>
+        <div class="col-3">
+            @yield('sidebar')
         </div>
     </div>
 </main>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,42 +11,97 @@
     </div>
 
     @if ($users->isNotEmpty())
-    <table class="table">
-        <thead class="thead-dark">
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Correo</th>
-            <th scope="col">Acciones</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($users as $user)
-        <tr>
-            <th scope="row">{{ $user->id }}</th>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-            <td>
-                @if ($user->trashed())
-                    <form action="{{ route('users.destroy', $user) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-link"><span class="oi oi-circle-x"></span></button>
-                    </form>
-                @else
-                    <form action="{{ route('users.trash', $user) }}" method="POST">
-                        @csrf
-                        @method('PATCH')
-                        <a href="{{ route('users.show', $user) }}" class="btn btn-link"><span class="oi oi-eye"></span></a>
-                        <a href="{{ route('users.edit', $user) }}" class="btn btn-link"><span class="oi oi-pencil"></span></a>
-                        <button type="submit" class="btn btn-link"><span class="oi oi-trash"></span></button>
-                    </form>
-                @endif
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
+    <div class="row row-filters">
+        <div class="col-md-6">
+            <form class="form-inline form-search">
+                <div class="input-group">
+                    <input type="search" class="form-control form-control-sm" placeholder="Buscar...">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-secondary btn-sm"><span class="oi oi-magnifying-glass"></span></button>
+                    </div>
+                </div>
+                &nbsp;
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Rol
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Todos</a>
+                        <a class="dropdown-item" href="#">Usuario</a>
+                        <a class="dropdown-item" href="#">Admin</a>
+                    </div>
+                </div>
+                &nbsp;
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Estado
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Todos</a>
+                        <a class="dropdown-item" href="#">Activo</a>
+                        <a class="dropdown-item" href="#">Inactivo</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+            
+        <div class="col-md-6 text-right">
+            <form class="form-inline form-dates">
+                <label for="date_start" class="form-label-sm">Fecha</label>&nbsp;
+                <div class="input-group">
+                    <input type="text" class="form-control form-control-sm" name="date_start" id="date_start" placeholder="Desde">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-secondary btn-sm"><span class="oi oi-calendar"></span></button>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <input type="text" class="form-control form-control-sm" name="date_start" id="date_start" placeholder="Hasta">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-secondary btn-sm"><span class="oi oi-calendar"></span></button>
+                    </div>
+                </div>
+            </form>            
+        </div>
+    </div>
+
+    <div class="table-responsive-lg">
+        <table class="table table-striped table-sm">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col"># <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+                <th scope="col" class="sort-asc">Nombre <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+                <th scope="col">Correo <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+                <th scope="col" class="th-actions">Acciones</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <th scope="row">{{ $user->name }}</th>
+                <td>{{ $user->email }}</td>
+                <td class="text-right">
+                    @if ($user->trashed())
+                        <form action="{{ route('users.destroy', $user) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-link"><span class="oi oi-circle-x"></span></button>
+                        </form>
+                    @else
+                        <form action="{{ route('users.trash', $user) }}" method="POST">
+                            @csrf
+                            @method('PATCH')
+                            <a href="{{ route('users.show', $user) }}" class="btn btn-outline-secondary btn-sm"><span class="oi oi-eye"></span></a>
+                            <a href="{{ route('users.edit', $user) }}" class="btn btn-outline-secondary btn-sm"><span class="oi oi-pencil"></span></a>
+                            <button type="submit" class="btn btn-outline-danger btn-sm"><span class="oi oi-trash"></span></button>
+                        </form>
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
     @else
         <p>No hay usuarios registrados.</p>
     @endif
