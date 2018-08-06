dbdc94d06c4295b1e94bafc5f116e95e2544f6cb
Ramon Lapenta <ramono@gmail.com>
2018-08-06 17:25:22 -0600

    Filtros modificados

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,6 +12,22 @@
 
     @if ($users->isNotEmpty())
     <div class="row row-filters">
+        <div class="col-12">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" checked>
+                <label class="form-check-label" for="inlineRadio1">Todos</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                <label class="form-check-label" for="inlineRadio2">Solo activos</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+                <label class="form-check-label" for="inlineRadio3">Solo inactivos</label>
+            </div>
+        </div>
+    </div>
+    <div class="row row-filters">
         <div class="col-md-6">
             <form class="form-inline form-search">
                 <div class="input-group">
@@ -32,14 +48,31 @@
                     </div>
                 </div>
                 &nbsp;
-                <div class="btn-group">
+                <div class="btn-group drop-skills">
                     <button type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Estado
+                        Habilidades
                     </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Todos</a>
-                        <a class="dropdown-item" href="#">Activo</a>
-                        <a class="dropdown-item" href="#">Inactivo</a>
+                    <div class="drop-menu skills-list">
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="skill1">
+                            <label class="form-check-label" for="skill1">CSS</label>
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="skill2">
+                            <label class="form-check-label" for="skill2">Laravel Development</label>
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="skill3">
+                            <label class="form-check-label" for="skill3">Front End</label>
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="skill4">
+                            <label class="form-check-label" for="skill4">Bases de Datos</label>
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="skill5">
+                            <label class="form-check-label" for="skill5">Javascript</label>
+                        </div>
                     </div>
                 </div>
             </form>
@@ -79,9 +112,12 @@
             @foreach($users as $user)
             <tr>
                 <td>{{ $user->id }}</td>
-                <th scope="row">{{ $user->name }}</th>
+                <th scope="row">
+                    {{ $user->name }} 
+                    <span class="note">Registrado: {{ $user->created_at->format('d/m/Y') }}</span>
+                </th>
                 <td>{{ $user->email }}</td>
-                <td>{{ $user->role }}</td>
+                <td>{{ $user->role }} <i class="icon-info"></i></td>
                 <td class="text-right">
                     @if ($user->trashed())
                         <form action="{{ route('users.destroy', $user) }}" method="POST">
