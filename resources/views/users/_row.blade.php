92eb5a467f0d1efdc131654435faceb0d33a9e0b
Duilio Palacios <duilio@styde.net>
2018-08-09 15:41:38 +0100

    Temporarily simplify the users list view

--- resources/views/users/_row.blade.php
+++ resources/views/users/_row.blade.php
@@ -1,15 +1,15 @@
 <tr>
-    <td rowspan="2">{{ $user->id }}</td>
+    <td>{{ $user->id }}</td>
     <th scope="row">
         {{ $user->name }}
-        <span class="note">Nombre de Empresa</span>
     </th>
+    <td>Nombre de la empresa</td>
     <td>{{ $user->email }}</td>
     <td>{{ $user->role }}</td>
-    <td>
-        <span class="note">Registro: {{ $user->created_at->format('d/m/Y') }}</span>
-        <span class="note">Último login: {{ $user->created_at->format('d/m/Y') }}</span>
-    </td>
+    {{--<td>--}}
+        {{--<span class="note">Registro: {{ $user->created_at->format('d/m/Y') }}</span>--}}
+        {{--<span class="note">Último login: {{ $user->created_at->format('d/m/Y') }}</span>--}}
+    {{--</td>--}}
     <td class="text-right">
         @if ($user->trashed())
             <form action="{{ route('users.destroy', $user) }}" method="POST">
@@ -28,9 +28,9 @@
         @endif
     </td>
 </tr>
-<tr class="skills">
-    <td colspan="1">
-        <span class="note">{{ $user->profile->profession->title }}</span>
-    </td>
-    <td colspan="4"><span class="note">{{ $user->skills->implode('name', ', ') ?: 'Sin habilidades :(' }}</span></td>
-</tr>
+{{--<tr class="skills">--}}
+    {{--<td colspan="1">--}}
+        {{--<span class="note">{{ $user->profile->profession->title }}</span>--}}
+    {{--</td>--}}
+    {{--<td colspan="4"><span class="note">{{ $user->skills->implode('name', ', ') }}</span></td>--}}
+{{--</tr>--}}
