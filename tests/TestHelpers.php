65667fd4b5bbc38fa79c4866f8798ce2eafeafce
Duilio Palacios <duilio@styde.net>
2019-09-05 15:37:32 +0100

    Upgrade to Laravel 6.0

--- tests/TestHelpers.php
+++ tests/TestHelpers.php
@@ -2,13 +2,15 @@
 
 namespace Tests;
 
+use Illuminate\Support\Str;
+
 trait TestHelpers
 {
     protected function assertDatabaseEmpty($table, $connection = null)
     {
         $total = $this->getConnection($connection)->table($table)->count();
         $this->assertSame(0, $total, sprintf(
-            "Failed asserting the table [%s] is empty. %s %s found.", $table, $total, str_plural('row', $total)
+            "Failed asserting the table [%s] is empty. %s %s found.", $table, $total, Str::plural('row', $total)
         ));
     }
 
@@ -17,7 +19,7 @@ trait TestHelpers
         $found = $this->getConnection($connection)->table($table)->count();
         $this->assertSame($expected, $found, sprintf(
             "Failed asserting the table [%s] has %s %s. %s %s found.",
-            $table, $expected, str_plural('row', $expected), $found, str_plural('row', $found)
+            $table, $expected, Str::plural('row', $expected), $found, Str::plural('row', $found)
         ));
     }
 
