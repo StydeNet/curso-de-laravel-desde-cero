83297da6100d0821ce473e0d190b3b287b06a3b8
Duilio Palacios <duilio@styde.net>
2020-07-01 12:33:21 +0100

    Move user filters and table to Livewire component

--- app/Http/Livewire/UsersList.php
+++ app/Http/Livewire/UsersList.php
@@ -6,8 +6,44 @@ use Livewire\Component;
 
 class UsersList extends Component
 {
+    /**
+     * @var mixed
+     */
+    protected $view;
+    /**
+     * @var mixed
+     */
+    protected $users;
+    /**
+     * @var mixed
+     */
+    protected $skills;
+    /**
+     * @var mixed
+     */
+    protected $checkedSkills;
+    /**
+     * @var mixed
+     */
+    protected $sortable;
+
+    public function mount($view, $users, $skills, $checkedSkills, $sortable)
+    {
+        $this->view = $view;
+        $this->users = $users;
+        $this->skills = $skills;
+        $this->checkedSkills = $checkedSkills;
+        $this->sortable = $sortable;
+    }
+
     public function render()
     {
-        return view('users._livewire-list');
+        return view('users._livewire-list', [
+            'view' => $this->view,
+            'users' => $this->users,
+            'skills' => $this->skills,
+            'checkedSkills' => $this->checkedSkills,
+            'sortable' => $this->sortable,
+        ]);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,35 +17,15 @@
         </p>
     </div>
 
-    @livewire('users-list')
+    @livewire('users-list', compact([
+        'view',
+        'users',
+        'skills',
+        'checkedSkills',
+        'sortable',
+    ]))
 
-    @includeWhen($view == 'index', 'users._filters')
 
-    @if ($users->isNotEmpty())
-
-    <div class="table-responsive-lg">
-        <table class="table table-sm">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col"><a href="{{ $sortable->url('name') }}" class="{{ $sortable->classes('name') }}">Nombre <i class="icon-sort"></i></a></th>
-                <th scope="col"><a href="{{ $sortable->url('email') }}" class="{{ $sortable->classes('email') }}">Correo <i class="icon-sort"></i></a></th>
-                <th scope="col"><a href="{{ $sortable->url('date') }}" class="{{ $sortable->classes('date') }}">Registrado el <i class="icon-sort"></i></a></th>
-                <th scope="col"><a href="{{ $sortable->url('login') }}" class="{{ $sortable->classes('login') }}">Último login <i class="icon-sort"></i></a></th>
-                <th scope="col" class="text-right th-actions">Acciones</th>
-            </tr>
-            </thead>
-            <tbody>
-                @each('users._row', $users, 'user')
-            </tbody>
-        </table>
-
-        {{ $users->links() }}
-        <p>Viendo página {{ $users->currentPage() }} de {{ $users->lastPage() }}</p>
-    </div>
-    @else
-        <p>No hay usuarios registrados.</p>
-    @endif
 @endsection
 
 @section('sidebar')
